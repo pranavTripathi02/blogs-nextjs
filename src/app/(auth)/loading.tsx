d61fd9ceb4b8c70c21c9a0d021893a94cf80f6bb
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-03-12 23:14:11 +0530

    folder org change

---
+++ src/app/(auth)/loading.tsx
@@ -0,0 +1,22 @@
+import { Skeleton } from "~/components/ui/skeleton";
+
+function Loading() {
+  return (
+    <div className="container my-2 h-fit min-w-[20rem] max-w-[30rem] space-y-8 rounded-md bg-background p-8">
+      <Skeleton className="h-[525px] w-full rounded-xl">
+        <div className="space-y-8 p-8">
+          <Skeleton className="mx-auto h-[25px] rounded-xl" />
+          <div className="mb-8 h-1" />
+          <Skeleton className="h-[25px] w-4/5 rounded-xl" />
+          <Skeleton className="h-[25px] w-4/5 rounded-xl" />
+          <Skeleton className="h-[25px] w-4/5 rounded-xl" />
+          <Skeleton className="h-[40px] w-1/5 rounded-lg" />
+          <Skeleton className="h-[10px] w-[150px] rounded-md" />
+          <Skeleton className="h-[10px] w-[150px] rounded-md" />
+        </div>
+      </Skeleton>
+    </div>
+  );
+}
+
+export default Loading;
---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,150 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
+import Link from "next/link";
+import { api } from "~/trpc/react";
+import { useRouter } from "next/navigation";
+import { useToast } from "~/components/ui/use-toast";
+
+const formSchema = z.object({
+  username: z
+    .string({ required_error: "Username is required" })
+    .min(1, { message: "Field cannot be empty" }),
+  password: z.string({ required_error: "Password is required" }).min(1, {
+    message: "Field cannot be empty",
+  }),
+});
+
+function Login() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const mutation = api.users.login.useMutation();
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const onFormSubmit = async (values: z.infer<typeof formSchema>) => {
+    const { username, password } = values;
+    console.log(values);
+    // const loginQuery = api.users.login.useQuery({ username, password });
+    const post = await mutation.mutateAsync(
+      { username, password },
+      {
+        onError: () => {
+          toast({
+            variant: "destructive",
+            title: "Uh oh! Something went wrong.",
+            description: "There was a problem with your request.",
+          });
+        },
+        onSuccess: () => {
+          toast({
+            title: "Success! You are now logged in.",
+            description: "Navigating back...",
+          });
+          // todo: login and navigate to homepage
+          // setTimeout(() => {
+          //   router.back();
+          // }, 5000);
+        },
+      },
+    );
+    console.log(post);
+    // form.reset();
+  };
+
+  return (
+    <div className="container my-2 h-fit min-w-[20rem] max-w-[30rem] space-y-8 rounded-md bg-background p-8">
+      <h1 className="text-xl">
+        Sign into your
+        <span className="bg-gradient-24 rounded-lg from-primary from-50% bg-clip-text px-1 tracking-tighter text-transparent">
+          nlogX
+        </span>
+        account
+      </h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onFormSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input
+                    autoComplete="username"
+                    placeholder="Username"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    autoComplete="off"
+                    placeholder="Password"
+                    type="password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="space-y-2">
+            <Button type="submit" size="lg" disabled={mutation.isLoading}>
+              Log in
+            </Button>
+            <Link className="block text-sm underline" href="#">
+              Forgot password?
+            </Link>
+          </div>
+          <div>
+            {mutation.isLoading ? (
+              <p>Logging into your account...</p>
+            ) : mutation.isError ? (
+              <p className="text-destructive">
+                Oops! An error occurred. Please try again
+              </p>
+            ) : null}
+          </div>
+        </form>
+      </Form>
+      <div className="text-sm">
+        <div>
+          <span>Don&apos;t have an account?</span>
+          <Link href="/register" className="ms-2 underline hover:text-primary">
+            Register
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
