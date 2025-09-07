f63bd18bb021e31244e8249098d08454ba750361
Pranav Tripathi <pranav02.tripathi@gmail.com>
2025-09-07 06:20:49 +0530

    feat: new trusted hostname added to next conf

--- next.config.js
+++ next.config.js
@@ -10,7 +10,19 @@ const config = {
     remotePatterns: [
       {
         // protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        // port: "",
+        // pathname: "/assets/**",
+      },
+      {
+        // protocol: "https",
         hostname: "images.unsplash.com",
+        // port: "",
+        // pathname: "/assets/**",
+      },
+      {
+        // protocol: "https",
+        hostname: "plus.unsplash.com",
         // port: "",
         // pathname: "/assets/**",
       },
